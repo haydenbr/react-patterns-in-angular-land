c7517b0250d7b113b0e01b0362858ea9b6de9724
Hayden Braxton <haydenbraxton@haydenbraxton.com>
2019-01-30 16:50:18 +0530

    tab button group with cva render prop

--- angular-quiz/src/app/shared/components/tab-button-group/tab-button-group.component.ts
+++ angular-quiz/src/app/shared/components/tab-button-group/tab-button-group.component.ts
@@ -1,5 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, ContentChildren, QueryList, forwardRef } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { AfterViewInit, Component, OnDestroy, ContentChildren, QueryList, Input } from '@angular/core';
 import { TimelineLite } from 'gsap';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
@@ -8,36 +7,16 @@ import { TabButtonComponent } from '../tab-button/tab-button.component';
 
 @Component({
 	selector: 'tab-button-group',
-	providers: [
-		{
-			provide: NG_VALUE_ACCESSOR,
-			useExisting: forwardRef(() => TabButtonGroupComponent),
-			multi: true,
-		},
-	],
 	styles: [`
 		:host { display: block; }
 	`],
 	template: `<ng-content select="tab-button"></ng-content>`,
 })
-export class TabButtonGroupComponent implements AfterViewInit, OnDestroy, ControlValueAccessor {
+export class TabButtonGroupComponent implements AfterViewInit, OnDestroy {
 	@ContentChildren(TabButtonComponent) private tabs = new QueryList<TabButtonComponent>();
-	onChange = (_: any) => {};
-	onTouched = () => {};
+	@Input() onChange = (_: any) => {};
 	private killSubscriptions = new Subject();
 	private _selectedTabValues: string[] = [];
-
-	writeValue(value: any) {
-		if (value) {
-			this.selectedTabValues = value;
-		}
-	}
-	registerOnChange(onChange: any) {
-		this.onChange = onChange;
-	}
-	registerOnTouched(onTouched: any) {
-		this.onTouched = onTouched;
-	}
 
 	get selectedTabValues() {
 		return this._selectedTabValues;
